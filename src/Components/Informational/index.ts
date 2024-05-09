f4c5c0381ed6a60f4bc52792858b493a80341643
Truls Haakenstad <truls.haakenstad@outlook.com>
2024-05-09 09:35:55 +0200

    Added settings, some assets and barrel files

---
+++ src/Components/Informational/index.ts
@@ -0,0 +1,1 @@
+export { default as Informational } from "./Informational";
---
+++ src/Components/index.ts
@@ -0,0 +1,4 @@
+export { Header } from "./Header/index";
+export { Hero } from "./Hero/index";
+export { Informational } from "./Informational/index";
+export { PetCards } from "./Card/index";
